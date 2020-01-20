987455e5690cda33cf9557d72cf6dac947cfd14f
tnishi <sgm00353@nifty.ne.jp>
2020-01-20 14:10:52 +0900

    [C] ファイル履歴一覧の画面から「パス」を非表示に

--- app/src/main/java/com/reliefoffice/pdic/FileHistorySelectionActivity.java
+++ app/src/main/java/com/reliefoffice/pdic/FileHistorySelectionActivity.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.CallSuper;
+import android.view.View;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -21,6 +22,8 @@ public class FileHistorySelectionActivity extends FileSelectionActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        textPath.setVisibility(View.GONE);
 
         fileHistory = new FileHistoryManager(this);
     }
